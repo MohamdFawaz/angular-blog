ad7074ba6b4a4034debbd4d330409e6682f09c9f
mohamdfawaz <mohamdfawaz93@gmail.com>
2020-01-03 18:24:47 +0200

    added show post component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+import { ShowPostComponent } from './show-post/show-post.component';
+
+const routes: Routes = [
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
